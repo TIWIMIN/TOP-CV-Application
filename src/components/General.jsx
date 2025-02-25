432d93645a2c3453f5bde9e71fb5292de6d5a818
TIWIMIN <tiwimin@gmail.com>
2025-02-24 22:32:17 -0800

    feat: Initialize General and EditButton components
    General component will serve to template the other sections of the CV. EditButton will be needed in the other sections of the CV.

---
+++ src/components/General.jsx
@@ -0,0 +1,12 @@
+import EditButton from "./EditButton";
+export default function General() {
+  return (
+    <>
+      <h2>General Information</h2>
+      <EditButton/>
+      <h3>Name</h3>
+      <h3>Email</h3>
+      <h3>Phone Number</h3>
+    </>
+  );
+}
